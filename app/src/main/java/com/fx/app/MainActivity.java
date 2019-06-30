440973f5874c2fbc3b790bb86c636b77ce46ce35
fx <simonov.vadim95@gmail.com>
2019-07-01 02:15:28 +0600

    Initial commit

--- app/src/main/java/com/fx/app/MainActivity.java
+++ app/src/main/java/com/fx/app/MainActivity.java
@@ -1,13 +1,35 @@
 package com.fx.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fx.switcher.Switcher;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switcher switcher = findViewById(R.id.mySwitch);
+        switcher.setSwitchBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        switcher.setState(true);
+        switcher.setOnClickSwitch(new Switcher.OnClickSwitch() {
+
+            @Override
+            public void switchOn() {
+                System.out.println("On");
+            }
+
+            @Override
+            public void switchOff() {
+                System.out.println("Off");
+            }
+        });
+
+
     }
 }
